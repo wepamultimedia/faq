519a37c794ecf65ded5530ab533c51297b8807be
Carlos Puche <cpuche@wepa.es>
2023-07-14 19:11:59 +0100

    add es languages

--- resources/lang/es/question-answer.php
+++ resources/lang/es/question-answer.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-
+    'asked_questions_list' => 'Lista de preguntas',
+    'asked_questions' => 'Preguntas',
+    'edit_title' => 'Edit pregunta',
+    'create_title' => 'Create pregunta',
+    'create_summary' => '...',
+    'delete_title' => 'Borrar pregunta',
+    'select_category' => 'Seleccionar categería',
 ];
