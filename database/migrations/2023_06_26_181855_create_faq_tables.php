14339f5b4922bf0e0e187214651058b87063abbf
Carlos Puche <cpuche@wepa.es>
2024-03-26 17:14:55 +0000

    Update 2023_06_26_181855_create_faq_tables.php

--- database/migrations/2023_06_26_181855_create_faq_tables.php
+++ database/migrations/2023_06_26_181855_create_faq_tables.php
@@ -8,48 +8,56 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('faq_categories', function (Blueprint $table) {
-            $table->id();
-            $table->integer('position');
-            $table->timestamps();
-        });
+        if(!Schema::hasTable('faq_categories')) {
+            Schema::create('faq_categories', function (Blueprint $table) {
+                $table->id();
+                $table->integer('position');
+                $table->timestamps();
+            });
+        }
 
-        Schema::create('faq_categories_translations', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('category_id');
-            $table->string('name');
+        if(!Schema::hasTable('faq_categories_translations')) {
+            Schema::create('faq_categories_translations', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('category_id');
+                $table->string('name');
 
-            $table->string('locale');
-            $table->unique(['locale', 'category_id']);
+                $table->string('locale');
+                $table->unique(['locale', 'category_id']);
 
-            $table->foreign('category_id')
-                ->references('id')
-                ->on('faq_categories')
-                ->cascadeOnDelete();
-        });
+                $table->foreign('category_id')
+                    ->references('id')
+                    ->on('faq_categories')
+                    ->cascadeOnDelete();
+            });
+        }
 
-        Schema::create('faq_qas', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('category_id');
-            $table->boolean('draft')->default(false);
-            $table->integer('position');
-            $table->timestamps();
-        });
+        if(!Schema::hasTable('faq_qas')) {
+            Schema::create('faq_qas', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('category_id');
+                $table->boolean('draft')->default(false);
+                $table->integer('position');
+                $table->timestamps();
+            });
+        }
 
-        Schema::create('faq_qas_translations', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('qa_id');
-            $table->string('question');
-            $table->text('answer');
+        if(!Schema::hasTable('faq_qas_translations')) {
+            Schema::create('faq_qas_translations', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('qa_id');
+                $table->string('question');
+                $table->text('answer');
 
-            $table->string('locale');
-            $table->unique(['locale', 'qa_id']);
+                $table->string('locale');
+                $table->unique(['locale', 'qa_id']);
 
-            $table->foreign('qa_id')
-                ->references('id')
-                ->on('faq_qas')
-                ->cascadeOnDelete();
-        });
+                $table->foreign('qa_id')
+                    ->references('id')
+                    ->on('faq_qas')
+                    ->cascadeOnDelete();
+            });
+        }
     }
 
     public function down()
